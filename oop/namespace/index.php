8efcabe6a8a45c8b6ffb614554ea0a8857ee21aa
Mohzu <mochzuhdi04@gmail.com>
2024-10-13 17:36:55 +0700

    Video 17 Namespace

---
+++ oop/namespace/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Si Juki Anak Kosan #1", "Faza Meonko", "Falcon Publishing", 66600, 100);
+// $produk2 = new Game("FC24", "Electronic Arts", "EA Sports", 239000, 50 );
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk( $produk1 );
+// $cetakProduk->tambahProduk( $produk2 );
+// echo $cetakProduk->cetak();
+
+// echo "<hr>";
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser;
+
+
+?>
