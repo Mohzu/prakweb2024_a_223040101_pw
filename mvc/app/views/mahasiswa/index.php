f0768fbd933cc7f889185df1519c28393a82c53a
Mohzu <mochzuhdi04@gmail.com>
2024-10-20 00:40:32 +0700

    Video 11 Delete Data

--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -16,10 +16,12 @@
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach( $data['mhs'] as $mhs) :  ?>
-                 <li class="list-group-item d-flex justify-content-between align-items-center">
+                 <li class="list-group-item">
                     <?= $mhs['nama']; ?>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'
+                    ]; ?>" class="badge bg-danger float-end ms-1" onclick="return confirm('yakin?');">Hapus</a>
                     <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'
-                    ]; ?>" class="badge text-bg-primary">Detail</a>
+                    ]; ?>" class="badge bg-primary float-end ms-1">Detail</a>
                 
                  </li>
                 <?php endforeach; ?>
